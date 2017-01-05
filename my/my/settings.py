22c1c4ba37f90b2fa4dfe413007203c9ccb2ea78
app865858 <app865858@gmail.com>
2017-01-05 20:46:27 +0800

    no 9

--- my/my/settings.py
+++ my/my/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'main',
     'article',
+    'share',
 ]
 
 MIDDLEWARE = [
--- my/my/urls.py
+++ my/my/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
  url(r'^admin/', admin.site.urls),
  url(r'^article/', include('article.urls', namespace='article')),
  url(r'^main/', include('main.urls', namespace='main')),
+ url(r'^share/',include('share.urls', namespace='share')),
  url(r'^.*', include('main.urls')),
 ]
